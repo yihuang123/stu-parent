fd5d39035f0eebef9b2140328ad21c56456906f9
huangyi <514649618@qq.com>
2021-12-25 22:12:38 +0800

    Initial commit

---
+++ stu-start/src/main/java/com/stu/StuStartApplication.java
@@ -0,0 +1,13 @@
+package com.stu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuStartApplication.class, args);
+    }
+
+}

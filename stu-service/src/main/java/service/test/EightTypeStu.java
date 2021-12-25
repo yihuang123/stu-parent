990b8eaf9227ca17186d79693b1c76eb300e3d45
huangyi <514649618@qq.com>
2021-12-25 23:05:24 +0800

    八大基本类型学习

---
+++ stu-service/src/main/java/service/test/EightTypeStu.java
@@ -0,0 +1,50 @@
+package service.test;
+
+public class EightTypeStu {
+
+
+    public static void main(String[] args) {
+
+        String str = "huangyi";
+
+        int num = 0;
+        Integer num2 = 300;
+        Integer i = num + num2;
+        System.out.println(num + num2);
+
+        long longNum = 200;
+
+        boolean bool = true;
+
+        double doubleNum = 0.3333333;
+
+        float floatNum = 0.3f;
+
+        char charStr = 'c';
+
+        short shortNum = 1;
+
+        //包装类型
+        Integer integer1  = 1;
+        Integer integer2  = 1;
+        System.out.println(integer1.equals(integer2));
+        Integer integer3  = 1111;
+        Integer integer4  = 1111;
+        System.out.println(integer3.equals(integer4));
+
+        Long numLong = 200L;
+
+        Boolean boo = false;
+
+        Double d = 2d;
+
+
+
+
+
+
+    }
+
+
+
+}
